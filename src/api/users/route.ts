6cd47a1bd3345fb4af46e7fea56e47319b08c5e5
cherman27 <carterherman27@gmail.com>
2025-09-19 08:58:26 -0400

    feat(24): Created User controller, router nad instantiate global prisma client

---
+++ src/api/users/route.ts
@@ -0,0 +1,39 @@
+import { UserController } from '@/lib/controllers/user.controller';
+import { ValidationError } from '@/lib/schemas/user.schema';
+import { NextRequest, NextResponse } from 'next/server';
+
+const userController = new UserController();
+
+export async function POST(request: NextRequest) {
+    try {
+        const body = await request.json();
+        const user = await userController.create(body);
+        return NextResponse.json(
+            {
+                success: true,
+                data: user,
+            },
+            { status: 200 }
+        );
+    } catch (err) {
+        if (err instanceof ValidationError) {
+            return NextResponse.json(
+                {
+                    success: false,
+                    err: err.message,
+                },
+                { status: 400 }
+            );
+        }
+
+        const message = err instanceof Error ? err.message : String(err);
+
+        return NextResponse.json(
+            {
+                success: false,
+                error: message,
+            },
+            { status: 500 }
+        );
+    }
+}
---
+++ src/lib/schemas/user.schema.ts
@@ -0,0 +1,24 @@
+import { z } from 'zod';
+
+export class ValidationError extends Error {
+    constructor(obj: any) {
+        super(
+            `The request structure is invalid. The server can't even parse the data.\n received the following: ${obj}`
+        );
+    }
+}
+
+export const createUserSchema = z.object({
+    name: z
+        .string()
+        .min(2, 'Name must be at least 2 characters')
+        .max(100, 'Name must be less than 100 characters')
+        .trim(),
+    email: z
+        .email('Invalid email format')
+        .toLowerCase()
+        .trim()
+        .max(255, 'Email must be less than 255 characters'),
+});
+
+export type CreateUserDTO = z.infer<typeof createUserSchema>;
