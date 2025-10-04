0f1dca2288efd918e864ee9438c29b51d56ef2c1
cherman27 <carterherman27@gmail.com>
2025-10-04 15:56:53 -0400

    feat: PUT and DELETE

---
+++ src/app/api/position/[id]/route.ts
@@ -0,0 +1,55 @@
+import { positionController } from '@/lib/controllers/position.controller';
+import { sargeApiError, sargeApiResponse } from '@/lib/responses';
+import { PositionNotFoundError } from '@/lib/schemas/position.schema';
+import { type NextRequest } from 'next/server';
+
+export async function GET(_request: NextRequest, { params }: { params: Promise<{ id: string }> }) {
+    try {
+        const userId = (await params).id;
+        const user = await positionController.get(userId);
+        return sargeApiResponse(user, 200);
+    } catch (error) {
+        if (error instanceof PositionNotFoundError) {
+            return sargeApiError(error.message, 404);
+        }
+
+        const message = error instanceof Error ? error.message : String(error);
+        return sargeApiError(message, 500);
+    }
+}
+
+export async function DELETE(
+    _request: NextRequest,
+    { params }: { params: Promise<{ id: string }> }
+) {
+    try {
+        const positionId = (await params).id;
+        const deletedPosition = await positionController.delete(positionId);
+        return sargeApiResponse(deletedPosition, 200);
+    } catch (error) {
+        if (error instanceof PositionNotFoundError) {
+            return sargeApiError(error.message, 404);
+        }
+
+        const message = error instanceof Error ? error.message : String(error);
+        return sargeApiError(message, 500);
+    }
+}
+
+export async function PUT(request: NextRequest, { params }: { params: Promise<{ id: string }> }) {
+    try {
+        const positionId = (await params).id;
+        const positionData = await request.json();
+        positionData.id = positionId;
+
+        const updatedPosition = await positionController.update(positionId, positionData);
+        return sargeApiResponse(updatedPosition, 200);
+    } catch (error) {
+        if (error instanceof PositionNotFoundError) {
+            return sargeApiError(error.message, 404);
+        }
+
+        const message = error instanceof Error ? error.message : String(error);
+        return sargeApiError(message, 500);
+    }
+}
