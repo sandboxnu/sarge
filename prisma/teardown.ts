bf5a929a5d486e29a3f3dd04c56ecc8346801af7
Meggan Shvartsberg <meggan567@gmail.com>
2025-10-08 13:38:12 -0400

    48/Better Seed Script (#50)
    * seed + teardown scripts
    
    * remove shell script + fix prettier
    
    * lint error

---
+++ prisma/teardown.ts
@@ -0,0 +1,35 @@
+/* eslint-disable no-console */
+
+import { PrismaClient } from '../src/generated/prisma';
+const prisma = new PrismaClient();
+
+async function main() {
+    await prisma.$executeRaw`
+  TRUNCATE TABLE
+    "Comment",
+    "Review",
+    "Task",
+    "Assessment",
+    "TaskTemplate",
+    "AssessmentTemplate",
+    "CandidatePoolEntry",
+    "Candidate",
+    "Position",
+    "Tag",
+    "UserRole",
+    "Role",
+    "User",
+    "Organization"
+  RESTART IDENTITY CASCADE;
+`;
+    console.log('Development database cleared.');
+}
+
+main()
+    .catch((e) => {
+        console.error('Teardown failed:', e);
+        process.exit(1);
+    })
+    .finally(async () => {
+        await prisma.$disconnect();
+    });
