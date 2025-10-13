cf824fccd7101f71eb6719123445c4f5e4776b8d
Laith Taher <lotaher04@gmail.com>
2025-10-13 15:48:32 -0400

    feat: taskTemplate routes (#47)

---
+++ src/app/api/taskTemplates/[id]/route.ts
@@ -0,0 +1,63 @@
+import { sargeApiError, sargeApiResponse } from '@/lib/responses';
+import { InvalidInputError } from '@/lib/schemas/errors';
+import taskTemplateController from '@/lib/controllers/taskTemplate.controller';
+import { TaskTemplateNotFoundError } from '@/lib/schemas/taskTemplate.schema';
+import { type NextRequest } from 'next/server';
+
+type TaskTemplateIdParam = {
+    params: {
+        id: string;
+    };
+};
+
+export async function GET(_req: NextRequest, { params }: TaskTemplateIdParam) {
+    try {
+        const foundTaskTemplate = await taskTemplateController.get(params);
+        return sargeApiResponse(foundTaskTemplate, 200);
+    } catch (error) {
+        if (error instanceof InvalidInputError) {
+            return sargeApiError(error.message, 400);
+        }
+
+        if (error instanceof TaskTemplateNotFoundError) {
+            return sargeApiError(error.message, 404);
+        }
+
+        const message = error instanceof Error ? error.message : String(error);
+        return sargeApiError(message, 500);
+    }
+}
+
+export async function DELETE(_req: NextRequest, { params }: TaskTemplateIdParam) {
+    try {
+        const deletedTaskTemplate = await taskTemplateController.delete(params);
+        return sargeApiResponse(deletedTaskTemplate, 200);
+    } catch (error) {
+        if (error instanceof InvalidInputError) {
+            return sargeApiError(error.message, 400);
+        }
+
+        if (error instanceof TaskTemplateNotFoundError) {
+            return sargeApiError(error.message, 404);
+        }
+
+        const message = error instanceof Error ? error.message : String(error);
+        return sargeApiError(message, 500);
+    }
+}
+
+export async function PUT(request: NextRequest, { params }: TaskTemplateIdParam) {
+    try {
+        const body = await request.json();
+        const idWithBody = { id: params.id, ...body };
+        const taskTemplate = await taskTemplateController.update(idWithBody);
+        return sargeApiResponse(taskTemplate, 200);
+    } catch (error) {
+        if (error instanceof InvalidInputError) {
+            return sargeApiError(error.message, 400);
+        }
+
+        const message = error instanceof Error ? error.message : String(error);
+        return sargeApiError(message, 500);
+    }
+}
---
+++ src/lib/schemas/taskTemplate.schema.ts
@@ -0,0 +1,63 @@
+import { z } from 'zod';
+
+export class TaskTemplateNotFoundError extends Error {
+    constructor() {
+        super('Task Template Not Found');
+    }
+}
+
+export class TaskTemplateExistsError extends Error {
+    constructor() {
+        super('Task Template with that name already exists');
+    }
+}
+
+const testCaseSchema = z.object({
+    input: z.string().min(1, 'Expected input required'),
+    output: z.string().min(1, 'Expected output required'),
+});
+
+export const getTaskTemplateSchema = z.object({
+    id: z.uuid(),
+});
+
+export const createTaskTemplateSchema = z
+    .object({
+        title: z.string().trim(),
+        content: z.string().min(2).max(500),
+        orgId: z.uuid(),
+        public_test_cases: z
+            .array(testCaseSchema)
+            .min(1, 'There must at least be one public test case'),
+        private_test_cases: z.array(testCaseSchema).default([]),
+    })
+    .strict();
+
+export const deleteTaskTemplateSchema = z.object({
+    id: z.uuid(),
+});
+
+export const updateTaskTemplateSchema = z.object({
+    id: z.uuid(),
+    title: z.string().trim(),
+    content: z.string().min(2).max(500),
+    public_test_cases: z
+        .array(testCaseSchema)
+        .min(1, 'There must at least be one public test case'),
+    private_test_cases: z.array(testCaseSchema).default([]),
+});
+
+export const TaskTemplateSchema = z.object({
+    id: z.uuid(),
+    title: createTaskTemplateSchema.shape.title,
+    content: createTaskTemplateSchema.shape.content,
+    public_test_cases: createTaskTemplateSchema.shape.public_test_cases,
+    private_test_cases: createTaskTemplateSchema.shape.private_test_cases,
+    orgId: createTaskTemplateSchema.shape.orgId,
+});
+
+export type TaskTemplateDTO = z.infer<typeof TaskTemplateSchema>;
+export type GetTaskTemplateDTO = z.infer<typeof getTaskTemplateSchema>;
+export type CreateTaskTemplateDTO = z.infer<typeof createTaskTemplateSchema>;
+export type DeleteTaskTemplateDTO = z.infer<typeof deleteTaskTemplateSchema>;
+export type UpdateTaskTemplateDTO = z.infer<typeof updateTaskTemplateSchema>;
