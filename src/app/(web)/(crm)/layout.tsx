4d7cc97a638607ce5c199d68dcfe60eac0f59960
cherman27 <carterherman27@gmail.com>
2025-10-21 22:15:54 -0400

    feat(60): Header fix: Header has auth context

--- src/app/(web)/(crm)/layout.tsx
+++ src/app/(web)/(crm)/layout.tsx
@@ -1,9 +1,7 @@
-import { UserProvider } from '@/lib/auth/auth-client';
-
 export default function CRMLayout({
     children,
 }: Readonly<{
     children: React.ReactNode;
 }>) {
-    return <UserProvider>{children}</UserProvider>;
+    return children;
 }
