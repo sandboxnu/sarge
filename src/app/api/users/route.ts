c04af8053927c652a1d551a4e80cdaefc320b09e
Bradford Derby <derby.b@northeastern.edu>
2025-10-22 19:13:58 -0400

    refactor controllers to services

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,12 +1,12 @@
-import { userController } from '@/lib/controllers/user.controller';
 import { type NextRequest } from 'next/server';
 import { sargeApiError, sargeApiResponse } from '@/lib/responses';
 import { InvalidInputError } from '@/lib/schemas/errors';
+import { createUser } from '@/lib/services/user.service';
 
 export async function POST(request: NextRequest) {
     try {
         const body = await request.json();
-        const user = await userController.create(body);
+        const user = await createUser(body);
         return sargeApiResponse(user, 200);
     } catch (error) {
         if (error instanceof InvalidInputError) {
