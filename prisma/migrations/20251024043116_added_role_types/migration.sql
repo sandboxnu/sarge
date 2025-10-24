3dabfa2fd2bcfa43d54593fb5b1aec8bd23474c6
Laith Taher <lotaher04@gmail.com>
2025-10-24 10:22:21 -0400

    feat: added roletype enum (#67)

---
+++ prisma/migrations/20251024043116_added_role_types/migration.sql
@@ -0,0 +1,15 @@
+/*
+  Warnings:
+
+  - Changed the type of `role` on the `Role` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
+
+*/
+-- CreateEnum
+CREATE TYPE "public"."RoleType" AS ENUM ('ADMIN', 'RECRUITER', 'REVIWER');
+
+-- DropIndex
+DROP INDEX "public"."Role_role_key";
+
+-- AlterTable
+ALTER TABLE "public"."Role" DROP COLUMN "role",
+ADD COLUMN     "role" "public"."RoleType" NOT NULL;
