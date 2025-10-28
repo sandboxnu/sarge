045d3a7164aa66cb753abc0a5cb77d86e708578c
Anzhuo-W <45926251+Anzhuo-W@users.noreply.github.com>
2025-10-28 18:46:29 -0400

    add satoshi variable

--- src/app/(web)/(crm)/dashboard/modal.tsx
+++ src/app/(web)/(crm)/dashboard/modal.tsx
@@ -4,7 +4,7 @@ export default function WelcomeModal() {
     return (
         <div className="bg-s-lightgrey bg-opacity-50 fixed inset-0 flex justify-center items-center">
             <div className="rounded-lg bg-white p-6 border border-[#E5E5E5] w-140 h-78 flex flex-col items-center justify-center">
-                <h2 className="mt-4 mb-2 text-xl font-bold text-black text-center">Welcome!</h2>
+                <h2 className="mt-4 mb-2 text-xl text-black text-center font-bold">Welcome!</h2>
                 <p className="text-black mb-4 text-base text-center">
                     Get started by creating or joining an organization. You&apos;ll be able to manage
                     tasks, assessments, and candidates all in one place.
@@ -13,7 +13,7 @@ export default function WelcomeModal() {
                     <button className="bg-s-purple mt-2 flex w-106 justify-center rounded-lg py-2">
                         Create Organization
                     </button>
-                    <button className="bg-white text-s-purple mt-2 mb-2 flex w-106 justify-center rounded-lg py-2">
+                    <button className="bg-white text-s-purple mt-1 mb-2 flex w-106 justify-center rounded-lg py-2">
                         Join Organization
                     </button>
                 </div>
