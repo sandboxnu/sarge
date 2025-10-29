ba7121642c53207091a02c8354913ca0f1b2422a
Anzhuo-W <45926251+Anzhuo-W@users.noreply.github.com>
2025-10-28 21:05:50 -0400

    make prettier

--- src/app/(web)/(crm)/dashboard/modal.tsx
+++ src/app/(web)/(crm)/dashboard/modal.tsx
@@ -2,18 +2,18 @@
 
 export default function WelcomeModal() {
     return (
-        <div className="bg-s-lightgrey bg-opacity-50 fixed inset-0 flex justify-center items-center">
-            <div className="rounded-lg bg-white p-6 border border-[#E5E5E5] w-140 h-78 flex flex-col items-center justify-center">
-                <h2 className="mt-4 mb-2 text-xl text-black text-center font-bold">Welcome!</h2>
-                <p className="text-black mb-4 text-base text-center">
-                    Get started by creating or joining an organization. You&apos;ll be able to manage
-                    tasks, assessments, and candidates all in one place.
+        <div className="bg-s-lightgrey bg-opacity-50 fixed inset-0 flex items-center justify-center">
+            <div className="flex h-78 w-140 flex-col items-center justify-center rounded-lg border border-[#E5E5E5] bg-white p-6">
+                <h2 className="mt-4 mb-2 text-center text-xl font-bold text-black">Welcome!</h2>
+                <p className="mb-4 text-center text-base text-black">
+                    Get started by creating or joining an organization. You&apos;ll be able to
+                    manage tasks, assessments, and candidates all in one place.
                 </p>
                 <div>
                     <button className="bg-s-purple mt-2 flex w-106 justify-center rounded-lg py-2">
                         Create Organization
                     </button>
-                    <button className="bg-white text-s-purple mt-1 mb-2 flex w-106 justify-center rounded-lg py-2">
+                    <button className="text-s-purple mt-1 mb-2 flex w-106 justify-center rounded-lg bg-white py-2">
                         Join Organization
                     </button>
                 </div>
