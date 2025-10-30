87aeb47f6ddf2ea68dc743e9ae5990f3e7b0f5ba
Anzhuo-W <45926251+Anzhuo-W@users.noreply.github.com>
2025-10-29 20:24:58 -0400

    update modal to use new button component

--- src/app/(web)/(crm)/dashboard/modal.tsx
+++ src/app/(web)/(crm)/dashboard/modal.tsx
@@ -1,21 +1,31 @@
 'use client';
 
+import { Button } from "@/lib/components/Button";
+
 export default function WelcomeModal() {
     return (
-        <div className="bg-s-lightgrey bg-opacity-50 fixed inset-0 flex items-center justify-center">
-            <div className="flex h-78 w-140 flex-col items-center justify-center rounded-lg border border-[#E5E5E5] bg-white p-6">
+        <div className="bg-sarge-gray-50 bg-opacity-50 fixed inset-0 flex items-center justify-center">
+            <div className="flex h-78 w-140 flex-col items-center justify-center rounded-lg border border-sarge-gray-200 bg-white p-6">
                 <h2 className="mt-4 mb-2 text-center text-xl font-bold text-black">Welcome!</h2>
                 <p className="mb-4 text-center text-base text-black">
                     Get started by creating or joining an organization. You&apos;ll be able to
                     manage tasks, assessments, and candidates all in one place.
                 </p>
                 <div>
-                    <button className="bg-s-purple mt-2 flex w-106 justify-center rounded-lg py-2">
+                    <Button 
+                        className="mb-2 flex w-106 justify-center"
+                        size="default"
+                        variant="primary"
+                    >
                         Create Organization
-                    </button>
-                    <button className="text-s-purple mt-1 mb-2 flex w-106 justify-center rounded-lg bg-white py-2">
+                    </Button>
+                    <Button 
+                        className="flex w-106 justify-center"
+                        size="default"
+                        variant="tertiary"
+                    >
                         Join Organization
-                    </button>
+                    </Button>
                 </div>
             </div>
         </div>
