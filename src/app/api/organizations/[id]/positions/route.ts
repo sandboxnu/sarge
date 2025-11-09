12abebb63ec81746ef95282c7088f5e303410b76
Meggan Shvartsberg <meggan567@gmail.com>
2025-11-09 15:21:04 -0500

    Get Positions By OrgId (#95)
    * done i think
    
    * fix prettier
    
    * fixes

--- src/app/api/organizations/[id]/positions/route.ts
+++ src/app/api/organizations/[id]/positions/route.ts
@@ -1,12 +1,21 @@
 import { type NextRequest } from 'next/server';
 import PositionService from '@/lib/services/position.service';
-import { handleError } from '@/lib/utils/errors.utils';
+import { handleError, notFound } from '@/lib/responses';
+import { prisma } from '@/lib/prisma';
 
 export async function GET(_request: NextRequest, { params }: { params: Promise<{ id: string }> }) {
     try {
         const orgId = (await params).id;
+        const org = await prisma.organization.findUnique({
+            where: {
+                id: orgId,
+            },
+        });
+        if (!org) {
+            return Response.json(notFound('Organization', orgId));
+        }
         const positionsResult = await PositionService.getPositionByOrgId(orgId);
-        return Response.json({ data: positionsResult }, { status: 200 });
+        return Response.json(positionsResult);
     } catch (err) {
         return handleError(err);
     }
