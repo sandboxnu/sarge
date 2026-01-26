e9c35f7e0641ab135d9ad25a565421ce7a93cc82
bderbs <bderbs@users.noreply.github.com>
2026-01-25 21:45:05 -0500

    update styling and formatting config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,15 +1,29 @@
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: ["./src/**/*.{ts,tsx,js,jsx,mdx}", "./app/**/*.{ts,tsx,js,jsx,mdx}", "./src/app/**/*.{ts,tsx,js,jsx,mdx}"] ,
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['var(--font-sans)', 'var(--font-inter)', 'ui-sans-serif', 'system-ui', '-apple-system', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial'],
-      },
+    content: [
+        './src/**/*.{ts,tsx,js,jsx,mdx}',
+        './app/**/*.{ts,tsx,js,jsx,mdx}',
+        './src/app/**/*.{ts,tsx,js,jsx,mdx}',
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: [
+                    'var(--font-sans)',
+                    'var(--font-inter)',
+                    'ui-sans-serif',
+                    'system-ui',
+                    '-apple-system',
+                    'Segoe UI',
+                    'Roboto',
+                    'Helvetica Neue',
+                    'Arial',
+                ],
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
 
 export default config;
