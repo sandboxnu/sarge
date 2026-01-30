016774727e9045d49dcbc2007eef10522a9c3b1e
Laith Taher <lotaher04@gmail.com>
2026-01-30 12:57:23 -0500

    feat: new many to many table for templates to account for question ordering (#152)

---
+++ prisma/migrations/20260130175448_added_indexes_to_title_field_for_templates/migration.sql
@@ -0,0 +1,5 @@
+-- CreateIndex
+CREATE INDEX "AssessmentTemplate_title_idx" ON "public"."AssessmentTemplate"("title");
+
+-- CreateIndex
+CREATE INDEX "TaskTemplate_title_idx" ON "public"."TaskTemplate"("title");
