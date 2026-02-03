42c859001ff708c00a38690e2f9d1f99909b1d3c
cherman27 <carterherman27@gmail.com>
2026-02-03 15:44:14 -0500

    fix search bar

--- src/lib/components/core/Search.tsx
+++ src/lib/components/core/Search.tsx
@@ -1,19 +1,19 @@
 import { SearchIcon } from 'lucide-react';
 import { Input } from '@/lib/components/ui/Input';
+import { type InputHTMLAttributes } from 'react';
 
-export interface SearchProps {
+type SearchProps = InputHTMLAttributes<HTMLInputElement> & {
     className?: string;
-    placeholder?: string;
-}
+};
 
-export function Search({ className, placeholder }: SearchProps) {
+export function Search({ className, ...inputProps }: SearchProps) {
     return (
         <div className="relative h-full w-full">
             <SearchIcon className="text-sarge-gray-500 absolute top-1/2 left-3 size-5 -translate-y-1/2" />
             <Input
                 type="text"
-                placeholder={placeholder ?? 'Type to search'}
-                className={`text-sarge-gray-500 h-11 w-full rounded-lg bg-white pl-10 italic ${className}`}
+                {...inputProps}
+                className={`h-11 w-full rounded-lg bg-white pl-10 italic ${className ?? ''}`}
                 aria-label="Search"
             />
         </div>
