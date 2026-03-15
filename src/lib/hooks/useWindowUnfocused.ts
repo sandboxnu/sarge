2e4e8b878a9be62533a7c4094beab4f1b43b68af
mshvartsberg <meggan567@gmail.com>
2026-03-15 15:36:46 -0400

    fix prettier lol

--- src/lib/hooks/useWindowUnfocused.ts
+++ src/lib/hooks/useWindowUnfocused.ts
@@ -1,39 +1,39 @@
 'use client';
 
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
 export function useWindowUnfocused(): boolean {
-  const [isWindowUnfocused, setIsWindowUnfocused] = useState(false);
+    const [isWindowUnfocused, setIsWindowUnfocused] = useState(false);
 
-  useEffect(() => {
-    const getIsUnfocused = () => {
-      return document.hidden || !document.hasFocus();
-    };
+    useEffect(() => {
+        const getIsUnfocused = () => {
+            return document.hidden || !document.hasFocus();
+        };
 
-    const handleFocus = () => {
-      setIsWindowUnfocused(getIsUnfocused());
-    };
+        const handleFocus = () => {
+            setIsWindowUnfocused(getIsUnfocused());
+        };
 
-    const handleBlur = () => {
-      setIsWindowUnfocused(true);
-    };
+        const handleBlur = () => {
+            setIsWindowUnfocused(true);
+        };
 
-    const handleVisibilityChange = () => {
-      setIsWindowUnfocused(getIsUnfocused());
-    };
+        const handleVisibilityChange = () => {
+            setIsWindowUnfocused(getIsUnfocused());
+        };
 
-    setIsWindowUnfocused(getIsUnfocused());
+        setIsWindowUnfocused(getIsUnfocused());
 
-    window.addEventListener("focus", handleFocus);
-    window.addEventListener("blur", handleBlur);
-    document.addEventListener("visibilitychange", handleVisibilityChange);
+        window.addEventListener('focus', handleFocus);
+        window.addEventListener('blur', handleBlur);
+        document.addEventListener('visibilitychange', handleVisibilityChange);
 
-    return () => {
-      window.removeEventListener("focus", handleFocus);
-      window.removeEventListener("blur", handleBlur);
-      document.removeEventListener("visibilitychange", handleVisibilityChange);
-    };
-  }, []);
+        return () => {
+            window.removeEventListener('focus', handleFocus);
+            window.removeEventListener('blur', handleBlur);
+            document.removeEventListener('visibilitychange', handleVisibilityChange);
+        };
+    }, []);
 
-  return isWindowUnfocused;
+    return isWindowUnfocused;
 }
