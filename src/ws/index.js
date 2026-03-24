50329d703e14f228e709494fb74c09d8919a9703
cherman27 <carterherman27@gmail.com>
2026-03-23 20:48:57 -0400

    feat: welcome to sarge, websockets (node)

--- src/ws/index.js
+++ src/ws/index.js
@@ -3,9 +3,7 @@ import { WebSocketServer } from 'ws';
 const ws = new WebSocketServer({ port: 8080 });
 
 ws.on('connection', (ws) => {
-    let message;
     ws.on('message', (data) => {
-        message = data;
+        ws.send(data.toString('utf-8'));
     });
-    ws.send(message);
 });
