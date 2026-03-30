5a0585c4484b60561cee681133dc17a7c36a3834
Laith Taher <lotaher04@gmail.com>
2026-03-29 22:11:03 -0400

    init WS logic

--- src/ws/index.js
+++
@@ -1,9 +0,0 @@
-import { WebSocketServer } from 'ws';
-
-const ws = new WebSocketServer({ port: 8080 });
-
-ws.on('connection', (ws) => {
-    ws.on('message', (data) => {
-        ws.send(data.toString('utf-8'));
-    });
-});
