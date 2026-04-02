c8d86b31c01def5f7c1e438075a9a99e0c41e022
Laith Taher <lotaher04@gmail.com>
2026-04-02 15:33:25 -0400

    lint

--- src/app/api/runner/[taskTemplateId]/route.ts
+++ src/app/api/runner/[taskTemplateId]/route.ts
@@ -3,7 +3,7 @@ import judge0Connector, {
 } from '@/lib/connectors/judge0.connector';
 import { SubmissionSchema } from '@/lib/schemas/submission.schema';
 import TaskTemplateService from '@/lib/services/task-template.service';
-import { getSession } from '@/lib/utils/auth.utils';
+// import { getSession } from '@/lib/utils/auth.utils';
 import { handleError } from '@/lib/utils/errors.utils';
 import { mapLanguageToJudge } from '@/lib/utils/language.utils';
 import { type NextRequest } from 'next/server';
@@ -19,7 +19,7 @@ export async function POST(
         const languageId = mapLanguageToJudge(parsed.language);
         const taskTemplate = await TaskTemplateService.getTaskTemplate(
             taskTemplateId,
-            'REPLACE THIS LATER LAITH WITH COOKIE'
+            'TODO: REPLACE THIS LATER LAITH WITH COOKIE'
         );
         const tests = [...taskTemplate.privateTestCases, ...taskTemplate.publicTestCases];
         const formatted: JudgeSubmissionRequestBody[] = tests.map((test) => ({
