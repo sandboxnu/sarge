93a733a446f5ba1124e33f8a567836f5481e3eac
Anzhuo-W <45926251+Anzhuo-W@users.noreply.github.com>
2026-04-08 22:41:20 -0400

    update outro

--- src/lib/components/assessment-flow/AssessmentOutro.tsx
+++ src/lib/components/assessment-flow/AssessmentOutro.tsx
@@ -8,29 +8,38 @@ type AssessmentOutroProps = {
 export default function AssessmentOutro({ assessment }: AssessmentOutroProps) {
     const handleCloseTab = () => {
         window.close();
+        // browser likely blocks it -> see javascript
+        // apparently hackerrank has a "you can safely close this tab" message so i put that too
+        alert('Your browser likely blocked the tab from closing. Please close this tab manually.');
     };
 
     return (
         <div className="bg-sarge-gray-50 flex h-full items-center justify-center p-8">
-            <div className="border-sarge-gray-200 bg-background flex w-full max-w-xl flex-col gap-6 rounded-2xl border p-10 shadow-sm">
+            <div className="border-sarge-gray-200 bg-background flex w-full max-w-2xl flex-col gap-6 rounded-2xl border p-10 shadow-sm">
                 <div>
                     <h1 className="text-sarge-gray-800 py-3 text-xl font-bold">
                         {assessment.assessmentTemplate.title}
                     </h1>
                     <div>
-                        <p className="mt-2 text-sm">
+                        <p className="mt-2 text-md">
                             Your submission has been received. The {assessment.organizationName}{' '}
                             team will be in touch with next steps within the next few weeks.
                         </p>
+                        <p className="mt-2 text-md">You can safely close this tab.</p>
                     </div>
                 </div>
-
-                <Button onClick={handleCloseTab} variant="primary">
+                
+                <div className="flex flex-col items-center">
+                <Button className="w-10 px-12 py-2" onClick={handleCloseTab} variant="primary">
                     Close Tab
                 </Button>
-                <p className="text-xs">
-                    For any questions, please reach out to the exam administrator at .
+                </div>
+                <p className="text-sm">
+                    For any questions, please reach out to the exam administrator at [email].
                 </p>
+                <div className="m-50">
+
+                </div>
             </div>
         </div>
     );
